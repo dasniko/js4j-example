1c0ff64af00fb6aa71a506f37c62bb5e2c8f9185
Niko Köbler <niko@n-k.de>
2017-04-27 11:34:28 +0200

    fix test

--- test/calcTest.js
+++ test/calcTest.js
@@ -4,7 +4,12 @@ import {Calculator} from '../src/calc';
 describe('Calculator', () => {
 
     it('should add the summands correctly', () => {
-        expect(Calculator.add(1,2)).toBe(3);
+        const calc = new Calculator();
+        expect(calc.add(1,2)).toBe(3);
+    });
+
+    it('should add the summands correctly (static call)', () => {
+        expect(Calculator.sum(1,2)).toBe(3);
     })
 
 });
